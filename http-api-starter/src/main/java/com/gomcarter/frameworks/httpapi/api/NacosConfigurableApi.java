e13ec773bf502a7cf82fe1dd05293fe2bcad3a77
gomcarter <gomcarter@vip.qq.com>
2019-12-20 11:27:06 +0800

    rename

--- http-api-starter/src/main/java/com/gomcarter/frameworks/httpapi/api/NacosConfigurableApi.java
+++ http-api-starter/src/main/java/com/gomcarter/frameworks/httpapi/api/NacosConfigurableApi.java
@@ -15,7 +15,7 @@ public abstract class NacosConfigurableApi extends BaseApi {
 
     private Map<String, Properties> propertiesMap = new HashMap<>();
 
-    public abstract NacosConfig getDiamondConfig();
+    public abstract NacosConfig getConfig();
 
     @Override
     protected Map<String, String> getUrlRouter() {
@@ -26,14 +26,14 @@ public abstract class NacosConfigurableApi extends BaseApi {
                 urlRouterMap.put((String) key, properties.getProperty((String) key));
             }
         } catch (IOException e) {
-            logger.error("初始化接口地址失败：{}", this.getDiamondConfig(), e);
+            logger.error("初始化接口地址失败：{}", this.getConfig(), e);
         }
 
         return urlRouterMap;
     }
 
     synchronized private Properties getProperties() throws IOException {
-        NacosConfig config = getDiamondConfig();
+        NacosConfig config = getConfig();
         String dataId = config.getDataId(),
                 group = config.getGroup(),
                 key = dataId + "_" + group;
